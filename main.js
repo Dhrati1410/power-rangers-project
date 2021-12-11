9faa347c828f5b53e4c9107fba4ee254854879fa
Dhrati1410 <84124855+Dhrati1410@users.noreply.github.com>
2021-12-11 15:58:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+var canvas = new fabric.Canvas('myCanvas');
+block_y = 1;
+block_x = 1;
+block_image_width = 300;
+block_image_height = 400;
+var block_image_object = "";
+
+function new_image(get_image) {
+fabric.Image.fromURL(get_image,
+            function (Img){
+            block_image_object = Img;
+            block_image_object.scaleToWidth(block_image_width);
+            block_image_object.scaleToHeight(block_image_height);
+            block_image_object.set({top: block_y, left: block_x});
+            canvas.add(block_image_object)
+            });
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+
+    if (keyPressed == '82')
+    {
+        block_x = -5;
+        new_image('rr.jpg');
+        console.log("r");
+        console.log(block_y = 1, block_x);
+    }
+    if (keyPressed == '71') {
+        block_x = 250;
+        new_image('gr.png');
+        console.log("g");
+        console.log(block_y = 1, block_x);
+    }
+
+    if (keyPressed == '89') {
+        block_x = 400;
+        new_image('yr.png');
+        console.log("y");
+        console.log(block_y = 1, block_x);
+    }
+    if (keyPressed == '80') {
+        block_x = 650;
+        new_image('pr.png');
+        console.log("p");
+        console.log(block_y = 1, block_x);
+    }
+    if (keyPressed == '66') {
+        block_x = 750;
+        new_image('br.png');
+        console.log("b");
+        console.log(block_y, block_x);
+    }
+
+}
